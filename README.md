14b8a8109ac497f8df3256dabce495c37eb47416
Carolin Axelsson <carolin.91@outlook.com>
2022-08-29 15:34:34 +0200

    gjot sista med javascript för matte

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # jamiesgames
+
+## Credits
+* För mattespelet, har jag tagit hjälp av denna video = https://www.youtube.com/watch?v=Uw9wc5wm_mw
--- matte.js
+++ matte.js
@@ -1,14 +1,14 @@
-const opt1 = document.getElementById("option1");
-const opt2 = document.getElementById("option2");
-const opt3 = document.getElementById("option3");
+const opt1 = document.getElementById("opt1");
+const opt2 = document.getElementById("opt2");
+const opt3 = document.getElementById("opt3");
 const audio = document.getElementById("myAudio");
 var answer = 0;
 
 function generate_equation(){
-    var num1 = Math.floor(Math.random() * 100)
-    var num2 = Math.floor(Math.random() * 100)
-    var dummyNum1 = Math.floor(Math.random() * 100)
-    var dummyNum2 = Math.floor(Math.random() * 100)
+    var num1 = Math.floor(Math.random() * 30)
+    var num2 = Math.floor(Math.random() * 30)
+    var dummyNum1 = Math.floor(Math.random() * 30)
+    var dummyNum2 = Math.floor(Math.random() * 30)
 
     var allAnswers = [];
     var switchAnswers = [];
@@ -18,9 +18,15 @@ function generate_equation(){
     document.getElementById("num1").innerHTML = num1;
     document.getElementById("num2").innerHTML = num2;
 
-    opt1.innerHTML = answer;
-    opt2.innerHTML = dummyNum1;
-    opt3.innerHTML = dummyNum2;
+    allAnswers = [answer, dummyNum1, dummyNum2];
+
+    for(i = allAnswers.length; i--;){
+        switchAnswers.push(allAnswers.splice(Math.random() * (i + 1), 1)[0]);
+    }
+
+    opt1.innerHTML = switchAnswers[0];
+    opt2.innerHTML = switchAnswers[1];
+    opt3.innerHTML = switchAnswers[2];
 }
 
 opt1.addEventListener("click", function(){
